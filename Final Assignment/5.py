bc2059676430007b7ab4e3a0d06869a66fa87a52
bleverbd <bleverbd@gmail.com>
2023-12-17 11:08:27 +0600

    5

---
+++ Final Assignment/5.py
@@ -0,0 +1,11 @@
+x=int(input("Enter a Number: "))
+y=int(input("Enter a Number: "))
+print("Before Sawping")
+print("x= ",x)
+print("y= ",y)
+x=x+y
+y=x-y
+x=x-y
+print("After Sawping")
+print("x= ",x)
+print("y= ",y)
