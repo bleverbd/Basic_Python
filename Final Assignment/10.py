887c0efff364d6d39c5f77ddcd6b3b341b858062
bleverbd <bleverbd@gmail.com>
2023-12-17 11:47:44 +0600

    10

--- Final Assignment/10.py
+++ Final Assignment/10.py
@@ -1,2 +1,9 @@
-x=int(input("Enter a integer Value: "))
-print("The integer Value is: ",x)
+x=int(input("Enter a integer value: "))
+y=int(input("Enter a integer value: "))
+z=int(input("Enter a integer value: "))
+if(x>y and x>z):
+    print("The Largest Number is: ",x)
+elif(y>x and y>z):
+    print("The Largest Number is: ",y)
+else:
+    print("The Largest Number is: ",z)
